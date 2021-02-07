e839632e1014ce7e04e7529046acd7d0d7189105
ceren esirgenci <ceren_esirgenci@hotmail.com>
2021-02-08 02:28:22 +0300

    8günödev

---
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Color entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Color entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color Get(System.Linq.Expressions.Expression<Func<Color, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Color> GetAll(System.Linq.Expressions.Expression<Func<Color, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Color entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1, BrandId=1, ColorId=1, DailyPrice=150, ModelYear=2010, Description="Manuel" },
+                new Car{Id=2, BrandId=2, ColorId=2, DailyPrice=185, ModelYear=2015, Description="Manuel" },
+                new Car{Id=3, BrandId=3, ColorId=3, DailyPrice=200, ModelYear=2018, Description="Otomatik" },
+                new Car{Id=4, BrandId=4, ColorId=4, DailyPrice=350, ModelYear=2029, Description="Otomatik" }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
