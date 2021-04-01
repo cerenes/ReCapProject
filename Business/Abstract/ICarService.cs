9225e33b04744fbe16d387d476097282cb1a8757
ceren esirgenci <ceren_esirgenci@hotmail.com>
2021-04-01 23:03:31 +0300

    18 gün ödev

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -12,11 +12,12 @@ namespace Business.Abstract
         IDataResult<List<Car>> GetAll();
         IDataResult<List<Car>>GetCarsByBrandId(int brandId);
         IDataResult<List<Car>> GetCarsByColorId(int colorId);
-        IDataResult<List<CarDetailDto>> GetCarDetails();
+        IDataResult<List<CarDetailDto>> GetCarDetails(int Id);
         IResult Add(Car car);
         IResult Update(Car car);
         IResult Delete(Car car);
         IDataResult<Car> GetById(int carId);
         IResult AddTransactionalTest(Car car);
+        
     }
 }
